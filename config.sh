082ef792b75f53c9e473adaf4bcb8d70cbac89d0
Kurt Kellner <kkellner@ecsteam.com>
2019-08-02 21:20:03 -0600

    gpu_mem

--- config.sh
+++ config.sh
@@ -13,6 +13,8 @@
 # cd rpi-setup
 # ./config.sh
 
+configFile=/boot/config.txt
+
 hostname=$1
 
 if [ -z "${hostname}" ]; then
@@ -34,13 +36,13 @@ sudo apt-get install -y i2c-tools
 
 sudo apt-get install -y python3-rpi.gpio
 
-sudo pip3 install -y RPI.GPIO
-sudo pip3 install -y adafruit-blinka
-sudo pip3 install -y adafruit-circuitpython-neopixel
-sudo pip3 install -y pyyaml
-sudo pip3 install -y schedule
-sudo pip3 install -y melopero-vl53l1x
-sudo pip3 install -y psutil
+sudo pip3 install RPI.GPIO
+sudo pip3 install adafruit-blinka
+sudo pip3 install adafruit-circuitpython-neopixel
+sudo pip3 install pyyaml
+sudo pip3 install schedule
+sudo pip3 install melopero-vl53l1x
+sudo pip3 install psutil
 
 
 # Add w1thermsensor command.
@@ -65,7 +67,12 @@ sudo apt-get clean
 
 sudo raspi-config nonint do_hostname "${hostname}"
 
-#sudo raspi-config nonint do_i2c %d
-#sudo raspi-config nonint do_spi %d
-#sudo raspi-config nonint do_onewire %d
+sudo raspi-config nonint do_i2c 0
+#sudo raspi-config nonint do_spi 0
+#sudo raspi-config nonint do_onewire 0
 #sudo raspi-config nonint do_memory_split %d
+
+if grep -Eq "^gpu_mem=" ${configFile}
+    echo "# Min memory to GPU to give to CPU"  >> ${configFile}
+    echo "gpu_mem=16" >> ${configFile}
+fi
